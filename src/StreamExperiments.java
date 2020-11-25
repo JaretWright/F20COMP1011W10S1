a77af7bdba5f9473129b8cbf7bcbff5adf65ce95
Jaret Wright <hkkA223>
2020-11-25 14:01:00 -0500

    Experimented with JSON

--- src/StreamExperiments.java
+++ src/StreamExperiments.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.OptionalDouble;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StreamExperiments {
     public static void main(String[] args) {
@@ -30,6 +34,23 @@ public class StreamExperiments {
             System.out.printf("The average age of customers is %.1f years%n",avgAge.getAsDouble());
 
 
+        //get the number of customers for each province
+        TreeMap<String, Integer> provinceCounts = DBUtility.getProvinceCounts();
+        System.out.println(provinceCounts);
+
+        //creating the same type of information (a map with province name and customer counts
+        Map<String, Long> provinceCountByStream = customers.stream()
+                                                    .map(Customer::getProvince)
+                                                    .collect(Collectors.groupingBy(Function.identity(),
+                                                                    Collectors.counting()));
+        System.out.println(provinceCountByStream);
+
+        //create a stream that returns the counts of each blood type
+        Map<String, Long> bloodTypesByStream = customers.stream()
+                                                    .map(Customer::getBloodType)
+                                                    .collect(Collectors.groupingBy(Function.identity(),
+                                                        Collectors.counting()));
+        System.out.println(bloodTypesByStream);
     }
 
     private static int getNumOfCustomerByGender(ArrayList<Customer> customers, String gender)
